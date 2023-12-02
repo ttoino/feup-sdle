593e82d8ec5e676abb7ed0f616b66454e484f531
Nuno Pereira <nunoafonso2002@gmail.com>
2023-12-02 22:00:24 +0000

    Added basic home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
     import "./style.css";
 </script>
 
-<div class="flex flex-col justify-center items-center min-h-full p-16">
+<main class="flex min-h-full flex-col items-center justify-center p-16">
     <slot />
-</div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
-<script lang="ts"></script>
+<script lang="ts">
+    type ShopingList = {
+        id: string;
+        name: string;
+    };
 
-<p>Hello world</p>
+    export let shoppingLists: ShopingList[] = [];
+</script>
+
+{#if shoppingLists.length > 0}
+    <main class="flex min-h-full flex-col items-center justify-center p-16">
+        <slot />
+    </main>
+{:else}
+    <h1 class="mb-2 text-center text-4xl font-bold">
+        You don't have any shopping lists yet.
+    </h1>
+    <h2 class="my-2 text-center text-2xl">Create one!</h2>
+
+    <form action="" class="flex flex-col items-center gap-4">
+        <label class="form-control">
+            <input
+                type="text"
+                name="name"
+                id="name"
+                class="input input-bordered"
+                placeholder="Name"
+            />
+        </label>
+        <button type="submit" class="btn btn-outline">Create</button>
+    </form>
+{/if}
