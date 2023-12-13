0899cfda9941e52bccb3fe40c518a0a0e9f3b5d5
Nuno Pereira <nunoafonso2002@gmail.com>
2023-12-13 15:16:54 +0000

    Removed unexistent modules from service-worker.js

--- client/src/service-worker/index.ts
+++ client/src/service-worker/index.ts
@@ -6,7 +6,6 @@ import { PUBLIC_SERVER_URL } from "$env/static/public";
 import type ShoppingList from "$lib/list";
 import setup from "$lib/localforage/setup";
 import * as listService from "$lib/service/list";
-import autoSync from "$lib/stores/sync";
 import { base, build, files, prerendered, version } from "$service-worker";
 
 const sw = self as unknown as ServiceWorkerGlobalScope;
@@ -71,16 +70,12 @@ sw.addEventListener("fetch", (event) => {
     event.respondWith(respondWithCachedAsset());
 });
 
-let autoSyncChanges = true;
-autoSync.subscribe((value) => (autoSyncChanges = value));
-
 const sync = async () => {
     if ((await sw.clients.matchAll()).length === 0) return (syncing = false);
 
-    if (autoSyncChanges)
-        await listService.syncAll(
-            (await listService.getAll()).filter((l) => l) as ShoppingList[],
-        );
+    await listService.syncAll(
+        (await listService.getAll()).filter((l) => l) as ShoppingList[],
+    );
 
     setTimeout(sync, 1000 * 5);
 };
